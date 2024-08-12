1ff34f44c95de2cd34759efa88256e02d001a9a7
toby <toby@aetherbird.app>
2024-08-12 01:37:05 -0400

    Finished parallax, finished particles.

--- scenes/game_objects/main_character.gd
+++ scenes/game_objects/main_character.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -750.0
 @onready var sprite_2d = $Sprite2D
+@export var particle : PackedScene
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,6 +12,7 @@ var jump_count = 0
 
 func jump():
 	velocity.y = JUMP_VELOCITY
+	spawn_particle()
 	
 func jump_side(x):
 	velocity.y = -900
@@ -18,23 +20,29 @@ func jump_side(x):
 	velocity.x = clamp(velocity.x, -1500, 1500)
 
 func _physics_process(delta):
-	# Animations
-	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "default"
 	
 	# Add the gravity.
 	if is_on_floor():
 		jump_count = 0
+		# Animations
+		if (velocity.x > 1 || velocity.x < -1):
+			sprite_2d.animation = "running"
+		else:
+			sprite_2d.animation = "default"
+		
 	else:
 		velocity.y += gravity * delta
-		sprite_2d.animation = "jumping"
+		if (jump_count == 2):
+			sprite_2d.animation = "double_jumping"
+		else:
+			sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jump_count < 2:
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
+		if (jump_count == 2):
+			spawn_particle()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -48,3 +56,10 @@ func _physics_process(delta):
 	
 	var isLeft = velocity.x < 0
 	sprite_2d.flip_h = isLeft
+
+func spawn_particle():
+	var particle_node = particle.instantiate()
+	particle_node.position = position
+	get_parent().add_child(particle_node)
+	await get_tree().create_timer(0.3).timeout
+	particle_node.queue_free()
